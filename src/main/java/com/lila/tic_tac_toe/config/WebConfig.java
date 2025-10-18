b7ccd734615f99de223adeb79aab92c970b99ff6
rakshithds <rakshi.ds50@gmail.com>
2025-10-18 17:53:03 +0530

    added cros cofigure to allow access to frontend after deployment

---
+++ src/main/java/com/lila/tic_tac_toe/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.lila.tic_tac_toe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    public WebMvcConfigurer crosConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("https://jade-flan-834694.netlify.app")
+                                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
